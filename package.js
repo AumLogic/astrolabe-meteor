20e855dfe7ced249694f3b275e36a3fc1e5e642b
adnan <mail@adnanwahab.com>
2013-06-29 14:54:11 -0700

    make package

---
+++ package.js
@@ -0,0 +1,18 @@
+Package.describe({
+  summary: "Tells you which queries and subscriptions are slow.",
+  internal: true
+});
+
+Package.on_use(function (api, where) {
+  where = where || ['client', 'server'];
+
+  api.use('d3', where);
+  api.use('underscore', where);
+  api.add_files(['profiler.js', 'dashboard.js'], where);
+});
+
+Package.on_test(function (api) {
+  // api.use('tinytest');
+  // api.use('profiler');
+  // api.add_files('deps_tests.js', 'client');
+});
---
+++ readme.md
@@ -0,0 +1,5 @@
+how to install?
+
+git clone git@github.com:adnan-wahab/meteor-profiler.git
+mv meteor-profiler <your-project-directory>/packages
+call dashboard()
