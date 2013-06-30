73c1a95aa5dfe9498bb747e85ec728e48e3457e9
adnan <mail@adnanwahab.com>
2013-06-29 22:11:18 -0700

    fakedata is now real but our db is too small  for it to measure anything

--- dashboard.js
+++ dashboard.js
@@ -7,36 +7,44 @@ function dash() {
       smallArea = "<div class='span4'></div>",
       largeArea = "<div class='span12'></div>";
 
-  $('h1').css('margin', '25 25 25 25');
-
   document.body.innerHTML = h1 + smallArea + largeArea;
-
-  function NumberLong (arg) {
-    return arg;
-  }
-
-  function ISODate (arg) {
-    return arg;
-  }
+  $('h1').css('margin', '25 25 25 25');
 
   var svg = d3.select(".span12")
             .append("svg")
             .attr("height", "1500px")
             .attr("width", "500px");
 
-  fakeData = fakeData
-             .sort(function (a, b) { return b.responseLength - a.responseLength})
-             .filter(function (d){ return d.responseLength;})
+  Meteor.setInterval(query_profile, 500);
 
-  var bars = svg.selectAll('rect')
-             .data(fakeData)
-             .enter()
-             .append('rect')
-             .attr('height', '50px')
-             .attr('width', function (d) { return d.responseLength})
-             .attr('fill', 'red')
-             .attr('y', function (d, i) { return i * 75; })
-             .attr('x', '50')
+  var o = _.once(function (data) {
+            data = JSON.parse(data)
+            console.log(_.pluck(data, 'lockstats').filter(_.identity))
+            console.log(JSON.stringify(data, null, 2))
+      })
+
+  function query_profile() {
+    Meteor.call('mongoProfile', function (err, data) {
+      update(JSON.parse(data))
+      data && o(data)
+    });
+  }
+
+  function update(data) {
+    data.filter(function (d){ return d.responseLength;})
+    .sort(function (a, b) { return b.responseLength - a.responseLength})
+
+    svg.selectAll('rect')
+               .data(data)
+               .enter()
+               .append('rect')
+               .attr('height', '50px')
+               .attr('width', function (d) { return d.responseLength})
+               .attr('fill', 'red')
+               .attr('y', function (d, i) { return i * 75; })
+               .attr('x', '50')
+  }
+
 
   Meteor.call('os', function (err, data) {
     var text = 'memoryUsage: ' +
@@ -44,12 +52,6 @@ function dash() {
       '/ ' +
       ~~ (data.totalmem / 1e6) + 'mb'
     d3.select('body').append('div').text(text)
-  })
-
-
-
-  Meteor.call('mongoProfile', function (err, data) {
-    console.log('abc' + data)
   })
 
 }
--- profiler.js
+++ profiler.js
@@ -1,23 +1,30 @@
-var Clouseau = Npm.require('clouseau');
+var log = console.log.bind(console)
+var MONGO_URL = process.env.MONGO_URL;
+var exec = Npm.require('child_process').exec;
+
+Meteor.setTimeout(function x() {
+  var url = MONGO_URL.replace('mongodb://', '');
+  exec('echo "db.setProfilingLevel(2)" | mongo ' + url, function () {
+    _.each(arguments, log);
+  })
+}, 5000)
 
 var Profiler = {
   mongoProfile: function() {
-    // var MongoDB = Npm.require('mongodb');
-    // var MongoClient = MongoDB.MongoClient;
-    // var MONGO_URL = process.env.MONGO_URL;
-    // var SYSTEM_PROFILE_COLLECTION = 'system.profile';
+    var MongoDB = Npm.require('mongodb');
+    var MongoClient = MongoDB.MongoClient;
+    var SYSTEM_PROFILE_COLLECTION = 'system.profile';
+    var Future = Npm.require('fibers/future')
+    var fut = new Future();
 
-    // var future = new Npm.require('fibers/future');
-    // for (var key in future) console.log(key)
+    MongoClient.connect(MONGO_URL, function(err, db) {
+      db.collection('system.profile').find({}).toArray(function(err, data) {
+        if (err) throw new Error(err);
+        return fut.ret(JSON.stringify(data));
+      });
+    });
 
-    // MongoClient.connect(MONGO_URL, function(err, db) {
-    //   db.collection('system.profile').find({}).toArray(function(err, data) {
-    //     if (err) throw new Error(err);
-    //     return future.ret(data);
-    //   })
-    // });
-
-    // return future.wait();
+    return fut.wait();
   }
 };
 
