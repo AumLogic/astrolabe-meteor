dcdbbb50f188249a0d5c4753d55c613560380996
Andreas Nauleau <andreas@adap.tv>
2013-06-29 18:01:14 -0700

    adds some basic mock up code to dashboard

--- dashboard.js
+++ dashboard.js
@@ -1,6 +1,21 @@
 dashboard = dash
 
 function dash() {
-  document.body.html = '';
-  console.log('test');
+  var h1        = "<h1>meteor profler - dashboard</h1>",
+      smallArea = "<div class='span4'></div>",
+      largeArea = "<div class='span12'></div>";
+
+  document.body.innerHTML = h1 + smallArea + largeArea;
+
+  var dataset = [10,20,30,50,50,100];
+
+  var svg = d3.select(".span12")
+              .append("svg")
+              .attr("height", "500px")
+              .attr("width", "500px");
+
+  var bars = svg.selectAll('rect')
+                .data(dataset)
+                .
+
 }
