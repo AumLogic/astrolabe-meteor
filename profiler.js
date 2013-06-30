21755924898fb17de0eed7bde0b29a9ee6aa25a0
CJ Winslow <whoaa512@gmail.com>
2013-06-29 19:32:08 -0700

    WIP: MongoProfiler

--- profiler.js
+++ profiler.js
@@ -1,3 +1,21 @@
-Meteor.setInterval(function (){
-  console.log('hello');
-}, 500);
+var Profiler = {
+  mongoProfile: function() {
+    var MongoDB = Npm.require('mongodb');
+    var MongoClient = MongoDB.MongoClient;
+    var MONGO_URL = process.env.MONGO_URL;
+    var SYSTEM_PROFILE_COLLECTION = 'system.profile';
+
+    MongoClient.connect(MONGO_URL, function(err, db) {
+      db.collection('system.profile')
+        .find({})
+        .toArray(function(err, res) {
+        if (err)
+          error(err);
+        // TODO: wrap res in a fiber to return all the logged mongo shit
+        console.log(res);
+      })
+    });
+  }
+};
+
+Meteor.methods(Profiler);
